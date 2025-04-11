1cd913b8e4a1f478a1a90c855353e9818341583c
navgurukul <laxmiyadav21@navgurukul.org>
2025-04-11 14:58:45 +0530

    add .gitignore file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,4 @@
-// require('dotenv').config(); // Load environment variables at the start
-// const app = require('./app');
 
-// const PORT = process.env.PORT || 5000;
-// console.log('MONGO_URI:', process.env.MONGO_URI); // Debugging line
-
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') }); // Load from backend/.env
